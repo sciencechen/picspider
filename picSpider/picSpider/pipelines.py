ccfda696cd54deebceb58752ee7bbc96c9d78a66
sciencechen <13790922187@139.com>
2021-05-08 23:22:40 +0800

    把图片保存转移到pipelines中处理

--- picSpider/picSpider/pipelines.py
+++ picSpider/picSpider/pipelines.py
@@ -14,24 +14,25 @@ from itemadapter import ItemAdapter
 
 
 class PicspiderPipeline:
-    picName = ""
     def process_item(self, item, spider):
-        print('xxxxxxxx-----------xxxxxxxxxxxxx----进入PicspiderPipeline')
         adapter = ItemAdapter(item)
-        print('xxxxxxxx------xxxxxxxxxxxxx----链接为：  '+adapter['picurl'], adapter['filename'])
-        # yield scrapy.Request(adapter['picurl'], adapter['filename'], callback=self.saveing)
+        picName = adapter['filename']
+        picFile = adapter['picurl']
+        with open("C:/chenjimiao/project/python/aiTeacherPlan/images4/" + '{}.jpg'.format(picName),
+                  'wb') as f:
+            f.write(picFile)
         print('xxxxxxxx-----------xxxxxxxxxxxxx----完成PicspiderPipeline')
         return item
 
-    def saveing(self, response):
-        # filename = '00.jpg'
-        # with open(filename, 'wb') as fp:
-        #     fp.write(response.body)
-        #     print('xxxxxxxx-----------xxxxxxxxxxxxx----完成saveing')
-
-        # self.picName = uuid.uuid1()
-        self.picName = response.item['filename']
-        with open("C:/chenjimiao/project/python/aiTeacherPlan/picspider/images2/" + '{}.jpg'.format(self.picName),
-                  'wb') as f:
-            f.write(response.body)
-            print('xxxxxxxx-----------xxxxxxxxxxxxx----完成saveing')
+    # def saveing(self, response):
+    #     # filename = '00.jpg'
+    #     # with open(filename, 'wb') as fp:
+    #     #     fp.write(response.body)
+    #     #     print('xxxxxxxx-----------xxxxxxxxxxxxx----完成saveing')
+    #
+    #     # self.picName = uuid.uuid1()
+    #     self.picName = response.item['filename']
+    #     with open("C:/chenjimiao/project/python/aiTeacherPlan/picspider/images2/" + '{}.jpg'.format(self.picName),
+    #               'wb') as f:
+    #         f.write(response.body)
+    #         print('xxxxxxxx-----------xxxxxxxxxxxxx----完成saveing')
--- picSpider/picSpider/spiders/pic_spider.py
+++ picSpider/picSpider/spiders/pic_spider.py
@@ -1,23 +1,17 @@
 
-import uuid
-
 import scrapy
-
 import sys
+import uuid
 print(sys.path)
 sys.path.append("..")
-import items
 from items import PicspiderItem
-
 
 
 class PicSpiderSpider(scrapy.Spider):
     name = 'pic_spider'
     allowed_domains = ['95mm.net', 'cdn.zzdaye.com']
-    start_urls = ['https://www.95mm.net/4879/2.html']
+    start_urls = ['https://www.95mm.net/2344/8.html']
     picName = 0
-    filename = '00.jpg'
-    temp_pic_url = []
 
     def parse(self, response):
         next_url = response.xpath("/html[1]//div[@class='nc-light-gallery']/p[1]/a[1]/@href").extract()
@@ -27,14 +21,10 @@ class PicSpiderSpider(scrapy.Spider):
         item = PicspiderItem()
         item['filename'] = self.picName[0].replace("/", "-")
         item['picurl'] = pic_url[0]
-        # yield item
-        # yield scrapy.Request(next_url[0], callback=self.parse)
-        # yield scrapy.Request(pic_url[0])
 
         # yield图片链接，然后保存
         yield scrapy.Request(pic_url[0], meta={'filename': item['filename']}, callback=self.saveing)
-        print(self.temp_pic_url)
-        self.temp_pic_url = []
+
         # 翻页，迭代，寻找下一个图片目标
         yield scrapy.Request(next_url[0], callback=self.parse)
         pass
@@ -42,10 +32,9 @@ class PicSpiderSpider(scrapy.Spider):
     def saveing(self, response):
         # self.picName = uuid.uuid1()
         self.picName = response.meta['filename']
-        with open("C:/chenjimiao/project/python/aiTeacherPlan/images3/" + '{}.jpg'.format(self.picName),
-                  'wb') as f:
-            f.write(response.body)
-            print('xxxxxxxx---------xxxxxxxx---------完成saveing')
+        item = PicspiderItem()
+        item['filename'] = self.picName
+        item['picurl'] = response.body
 
-
-
+        # yield进pipelines中处理
+        yield item
